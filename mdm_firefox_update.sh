5c2b8e7beedecb95f712bd8eed25dac954ef48b7
Jean-Carol Forato <jean-carol.forato@dataiku.com>
2022-05-20 12:24:32 +0200

    Use actual shell script

--- mdm_firefox_update.sh
+++ mdm_firefox_update.sh
@@ -2,66 +2,62 @@
 ############
 # This script installs/updates the latest version of the Firefox web browser.
 ############
+# 1.2 - Sandray Douglas / Jean-Carol Forato - 19-06-2022
+#        * Use shell script instead.
 # 1.1 - Sandray Douglas / Jean-Carol Forato - 19-06-2022
 #        * Use shell before calling the python interpreter.
 # 1.0 - Sandray Douglas - 19-06-2022
 #        * Initial script creation.
 ############
 
-PYTHON="python3"
-PYTHON="$(command -v "python3")"
-FOUND=$?
+# TODO use the $LANG env variable to fetch the proper language version
+# URL='https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US'
+# DEBUGGING URL
+# URL='https://download.mozilla.org/?product=firefox-nightly-latest-l10n-ssl&os=osx&lang=en-GB'
+OUT_DIR="/tmp"
+APP_NAME="Firefox"
+# DEBUGGING APP name
+# APP_NAME="Firefox Nightly"
+OUT_FILENAME="$APP_NAME.dmg"
+OUT_PATH="$OUT_DIR/$OUT_FILENAME"
 
-if ! PYTHON_LOC="$(command -v "$PYTHON")" || [[ -z $PYTHON_LOC ]]; then
-    echo "Looking for python2..."
-    PYTHON="python"
-    if ! PYTHON_LOC="$(command -v "$PYTHON")" || [[ -z $PYTHON_LOC ]]; then
-        echo "python could not be found on this machine! Aborting."; 
-        exit 1;
-    fi
-fi
+# [ -f "$OUT_PATH" ] && echo "Removing previously downloaded $OUT_FILENAME" && rm "$OUT_PATH";
 
-echo "Found python interpreter at ${PYTHON_LOC}"
+echo "Downloading $APP_NAME..."
+# curl -L -o $OUT_FILENAME --output-dir $OUT_DIR $URL --
 
-${PYTHON_LOC} - << END
-import os
-from distutils.dir_util import copy_tree
-from pathlib import Path
+echo "Downloaded file $OUT_FILENAME to $OUT_PATH.";
 
-url = "https://download.mozilla.org/?product=firefox-nightly-latest-l10n-ssl&os=osx&lang=en-GB"
-# server='https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US'
-output_dir = Path("/tmp/")
-# output_file = "firefox-102.0a1.en-GB.mac.dmg"
-output_base_filename = "Firefox.dmg"
-old_installer_path = output_dir / output_base_filename
+APP_DIR="$APP_NAME.app"
+INSTALLATION_PATH="/Applications"
+MOUNT_BASE="/Volumes/$APP_NAME"
+MOUNT_PATH="$MOUNT_BASE/$APP_DIR"
+INSTALLED_APP_PATH="$INSTALLATION_PATH/$APP_DIR"
 
-if old_installer_path.exists():
-    old_installer_path.unlink()
-    
-installed_app = Path("/Applications/Firefox.app")
+[ ! -d "$INSTALLED_APP_PATH" ] \
+&& echo "$INSTALLED_APP_PATH does not exist, $APP_NAME is probably not installed. Exiting." \
+&& exit 2;
 
-cmd = 'curl -L -o Firefox.dmg --output-dir \"{}\" {}'.format(str(output_dir), url)
-os.system(cmd)
+OUTPUT=$(hdiutil attach -nobrowse "$OUT_PATH")
+[ $? -eq 0 ] || exit 3;
 
-print("Firefox.dmg downloaded")
+MOUNTED="$(echo $OUTPUT | grep -i $APP_NAME | awk -F'\t' '{print $3}')";
+[ $? -ne 0 ] && echo "Failed to mount at $MOUNTED" && exit 4;
+echo "Mounted installation dmg at $MOUNTED.";
 
-exit(5)
+# FIXME after forcing the installation (below), Firefox fails to start and MacOS
+# detects it as being corrupted. Removing /Applications/Firefox.app and reinstalling
+# fixes the issue, but this should not happen in the first place.
 
-if installed_app.exists():
-    cmd = 'hdiutil attach -nobrowse -mountpoint "/Volumes/Firefox" "/tmp/Firefox.dmg"'
-    os.system(cmd)
-    copy_tree("/Volumes/Firefox/Firefox.app", "/Applications/Firefox.app")
-    cmd = 'hdiutil detach "/Volumes/Firefox"'
-    os.system(cmd)
-    cmd = 'rm /tmp/Firefox.dmg'
-    os.system(cmd)
-else:
-    cmd = 'hdiutil attach -nobrowse -mountpoint "/Volumes/Firefox" "/tmp/Firefox.dmg"'
-    os.system(cmd)
-    cmd = 'cp -R /Volumes/Firefox/Firefox.app /Applications/Firefox.app '
-    os.system(cmd)
-    cmd = 'hdiutil detach "/Volumes/Firefox"'
-    os.system(cmd)
-    cmd = 'rm /tmp/Firefox.dmg'
-    os.system(cmd)
-END
+# Force update the binaries by copying files from the installer (dmg) file
+cp -pRf "$MOUNT_PATH/" "$INSTALLED_APP_PATH";
+
+# User should be part of the admin group already
+# chown -R :admin "$INSTALLED_APP_PATH";
+# Allow the user to modify the files (for automatic updates)
+# chmod -R g+w "$INSTALLED_APP_PATH";
+
+[ $? -ne 0 ] && echo "Failed to copy new version to $INSTALLED_APP_PATH" && exit 5;
+
+hdiutil detach "$MOUNTED";
+# rm $OUT_PATH;
